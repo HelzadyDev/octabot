87ee83939844349b701b34a957625f8d571df69e
HelzadyDev <natanael.santos.play@gmail.com>
2024-10-19 23:40:02 -0300

    feat: Adiciona comando "ping" para verificação de latência
    - Implementado comando "ping" que responde com o tempo de ping da API e do gateway.
    - Utiliza reply e embeds para exibir informações de latência de forma amigável.
    - Adicionado um tempo de espera antes de exibir os resultados para simular o cálculo.
    - Movido arquivo do comando da pasta `commands` para a subpasta `slash` para melhor organização.

---
+++ src/discord/commands/public/slash/ping.ts
@@ -0,0 +1,43 @@
+import { Command } from "#base";
+import { embedReply, reply } from "#functions";
+import { settings } from "#settings";
+import { brBuilder } from "@magicyan/discord";
+import { ApplicationCommandType } from "discord.js";
+
+new Command({
+  name: "ping",
+  description: "Replies with pong 🏓",
+  type: ApplicationCommandType.ChatInput,
+  async run(interaction) {
+	await interaction.deferReply({ ephemeral });
+    const { client, guild } = interaction;
+
+    // Obitem o ping do gateway
+    const gatewayping = client.ws.ping;
+
+    // Mensagem inicial para calular o ping da api
+    const startTime = Date.now();
+    await reply.primary({
+    	interaction,
+		update: true,
+    	text: "Calculando o ping da API"
+    })
+    
+    // calculando o ping
+    const apiping = Date.now() - startTime;
+
+    setTimeout(async() => {
+		await embedReply({
+      interaction,
+      update: true,
+      color: settings.colors.primary,
+      embed: {
+        description: brBuilder(
+          `:ping_pong: | API Ping: ${apiping}ms`,
+          `:stopwatch: | Geteway Ping: ${gatewayping}ms`
+        ),
+      },
+    });
+	}, 2000);
+  },
+});
